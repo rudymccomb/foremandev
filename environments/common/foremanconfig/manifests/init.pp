8bb88255d30fca22465fcc637a2fd198569cb012
rudymccomb <rudy@thnkbig.com>
2014-09-22 18:43:46 -0400

    updated modules worked on with richard

--- environments/common/foremanconfig/manifests/init.pp
+++ environments/common/foremanconfig/manifests/init.pp
@@ -4,7 +4,8 @@ class foremanconfig {
         # Typically you'd install this everywhere
 	class { 'mco_profile::server':
 		middleware_hosts => [ 'localhost' ],
-	}
+	        connector        => 'redis',
+        }
 	# Allow user to run mcollective
         file { '/etc/mcollective/clients/frm2_client.pem':
         	source => '/root/.mcollective.d/credentials/certs/frm2_client.pem',
@@ -13,11 +14,15 @@ class foremanconfig {
         mcollective::common::setting { 'plugin.activemq.heartbeat_interval':
 		value => '30',
 	}
-        
+        # already declared in redis.pp for mcollective
+        #mcollective::common::setting { 'plugin.redis.host':
+        #        value => 'localhost',
+        #}
         # the mco command line tool
         # Typically just on a management host
         class { 'mco_profile::client':
                 middleware_hosts => [ 'localhost' ],
+                connector        => 'redis',
         }
 
 	# the central server all mcollective servers (agents) talk to
